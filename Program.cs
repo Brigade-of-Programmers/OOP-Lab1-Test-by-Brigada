fafede4306f1644ce3833e5d97379e178b417cd3
Vladyslava Kulhova <vladyslava.kulhova@gmail.com>
2022-10-23 20:56:23 +0200

    Small checkings

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
 
 namespace project{
 
@@ -6,15 +7,15 @@ namespace project{
         
         static void Main(){
             
-            Test test1 = new Test();
-            test1.AddQuestionToTest();
-            test1.AddQuestionToTest();
-            test1.AddQuestionToTest();
-            test1.DeleteQuestionFromTest();
-            for(int i = 0; i < 3; i++)
-            {
-                System.Console.WriteLine(test1[i] + i + " / ");
-            }
+            // Test test1 = new Test();
+            // test1.AddQuestionToTest();
+            // test1.AddQuestionToTest();
+            // test1.AddQuestionToTest();
+            // test1.DeleteQuestionFromTest();
+            // for(int i = 0; i < test1.QuestionsArr.Count; i++)
+            // {
+            //     System.Console.WriteLine(test1.QuestionsArr[i].QuestionText + " " + test1.QuestionsArr[i].Mark + " " + i + " \n ");
+            // }
         }
 
     }
