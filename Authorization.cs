54477137bdc765b83d492617f7f72fb0d4ef0eb6
Gery4 <nikita0288@gmail.com>
2022-10-23 17:52:09 +0300

    Added Login

--- Authorization.cs
+++ Authorization.cs
@@ -1,14 +1,38 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.IO;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public class Class1
+namespace TestingApp
 {
-	public Class1()
+	public class Authorization
 	{
-		//
-		// TODO: Add constructor logic here
-		//
+		public void Login()
+		{
+			string username, password, username1, password1 = string.Empty;
+
+			Console.Write("Enter a username: ");
+			username = Console.ReadLine();
+			Console.Write("Enter a password: ");
+			password = Console.ReadLine();
+
+			using (StreamReader sr = new StreamReader(File.Open("C:\\LoginInfo.txt", FileMode.Open)))
+			{
+				username1 = sr.ReadLine();
+				password1 = sr.ReadLine();
+				sr.Close();
+			}
+
+			if (username == username1 && password == password1)
+				Console.WriteLine("Login successful");
+			else
+				Console.WriteLine("Login failed");
+		}
+
+		public void Registration()
+        {
+
+        }
 	}
 }
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace TestingApp
+{
+     public class Program
+     {
+        static void Main()
+        {
+            Authorization auth = new Authorization();
+            auth.Login();
+            // todo: registration
+        }
+     }
+}
