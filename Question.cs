03efe7cc773ba617e09fdea0b0329e41ac16b6e6
Vladyslava Kulhova <vladyslava.kulhova@gmail.com>
2022-10-23 19:05:41 +0200

    Added accessors for private fields from class Question

--- Question.cs
+++ Question.cs
@@ -2,10 +2,34 @@ using System;
 
 namespace project{
 
-    class Question : Test{
+    class Question{
 
-        private string question;
+        private string questionText;
         private int mark;
+
+        public string QuestionText{
+
+            get{
+                return this.questionText;
+            }
+
+            set{
+                this.questionText = value;
+            }
+            
+        }
+
+        public int Mark{
+
+            get{
+                return this.mark;
+            }
+
+            set{
+                this.mark = value;
+            }
+            
+        }
 
     }
 
