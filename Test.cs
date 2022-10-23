dd1918ef779dca149b10ed2ed73313b609bbcef7
Vladyslava Kulhova <vladyslava.kulhova@gmail.com>
2022-10-23 15:19:14 +0200

    Made constructor for Test.cs and some methods without anything inside

--- Test.cs
+++ Test.cs
@@ -4,7 +4,21 @@ namespace project{
 
     class Test{
 
-        
+        private Question[] questions;
+        private int resultOfTest;
+
+        public Test (Question[] questions){
+            System.Console.WriteLine("Test has been created.");
+        }
+
+        public void AddQuestionToTest(){}
+
+        public void DeleteQuestionFromTest(){}
+
+        public void ChangePlaceInTest(){}
+
+        public void EditQuestionInTest(){}
+
 
     }
 
