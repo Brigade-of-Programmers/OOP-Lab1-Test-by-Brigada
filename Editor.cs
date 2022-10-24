4f21d1e240025188259bd3f51c4280a9502b57fd
Illia <illiamoryev@gmail.com>
2022-10-24 20:04:17 +0300

    Editor created

---
+++ Editor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using project;
+
+namespace project
+{
+    public class Editor
+    {
+        private List<Test> testArr = new List<Test>();
+
+        public List<Test> TestArr
+        {
+            get
+            {
+                return this.TestArr;
+            }
+        }
+
+        public void printAllTests()
+        {
+            Console.WriteLine("List of all tests: ");
+
+            foreach (Test item in TestArr)
+            {
+                Console.WriteLine(Test.NameOfTest);
+            }
+        }
+
+        //public void chooseTest()
+        //{
+        //    Console.WriteLine("Choose the number of test: ");
+        //    int index = Convert.ToInt32(Console.ReadLine());
+        //    TestArr[index].startTest;
+        //}
+
+        public void addTest()
+        {
+            Console.WriteLine("Choose test name: ");
+            string name = Console.ReadLine();
+            Test test = new Test(name);
+            TestArr.Add(test);
+        }
+
+        public void deleteTest()
+        {
+            Console.WriteLine("Choose the number of the test you want to delete: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+            TestArr.RemoveAt(index - 1);
+        }
+
+        public void editTestName()
+        {
+            Console.WriteLine("Choose the number of the test you want to delete: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose new test name: ");
+            string name = Console.ReadLine();
+
+            TestArr[index - 1].NameOfTest = name;
+        }
+    }
+}
+
+
