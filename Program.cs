02833571e256f92571db9e89e434430a43f124d9
Illia <illiamoryev@gmail.com>
2022-10-27 11:43:48 +0300

    testing1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace project{
     class Program{
         
         static void Main(){
-            
+            Editor editor = new Editor();
+
+            Test test1 = new Test();
+            test1.NameOfTest = "test1";
+            editor.TestArr.Add(test1);
+
+            Test test2 = new Test();
+            test2.NameOfTest = "test2";
+            editor.TestArr.Add(test2);
+
+            editor.printAllTests();
         }
 
     }
