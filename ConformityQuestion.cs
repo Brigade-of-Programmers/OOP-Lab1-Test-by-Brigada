5431af44e398e27d3a7ead199c994b06071e1e82
Illia <illiamoryev@gmail.com>
2023-01-09 14:59:41 +0200

    3 question type added

---
+++ ConformityQuestion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class ConformityQuestion : Question
+    {
+        private Dictionary<int, int> correctAnswers = new Dictionary<int, int>();
+        public int getAnswer(int numberOfQuestion)
+        {
+            int value;
+            correctAnswers.TryGetValue(numberOfQuestion, out value);
+            return value;
+        }
+        public void addAnswer(int numberOfQuestion, int answer)
+        {
+            correctAnswers.Add(numberOfQuestion, answer);
+        }
+
+        public void removeAnswer(int numberOfQuestion)
+        {
+            correctAnswers.Remove(numberOfQuestion);
+        }
+    }
+}
---
+++ ScaleQuestion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class ScaleQuestion : Question
+    {
+        private int scale;
+        public int Scale
+        {
+            get
+            {
+                return scale;
+            }
+
+            set
+            {
+                if (value >= 0 && value <= 5)
+                {
+                    this.scale = value;
+                }
+            }
+        }
+    }
+}
