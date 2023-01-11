6b2988efbdb0ebd4b3fa9d6c1fb2acef7080e3b7
Illia <illiamoryev@gmail.com>
2023-01-11 13:23:44 +0200

    editors changes

--- ConsoleEditor.cs
+++ ConsoleEditor.cs
@@ -6,21 +6,67 @@ using System.Threading.Tasks;
 
 namespace project
 {
-	class ConsoleEditor
+    class ConsoleEditor
     {
-        public void main()
+        public void runConsoleEditor()
         {
-            int regOrAuth = -1;
-            Console.WriteLine("© 2023. Programmer brigade.");
             Console.WriteLine("Welcome to the testing application.");
-            while (regOrAuth != 0 && regOrAuth != 1)
+            String regOrNot = "";
+            while (regOrNot != "1" && regOrNot != "2")
             {
-                Console.WriteLine("New one? Press 0");
-                Console.WriteLine("Already have an account? Press 1");
-                regOrAuth = Convert.ToInt32(Console.ReadLine());
-                if (regOrAuth != 0 && regOrAuth != 1) Console.WriteLine("Invalid input. Please try again.");
+                Console.WriteLine("New one? Press 2");
+                Console.WriteLine("Have an account? Press 1");
+                regOrNot = Console.ReadLine();
+                if (regOrNot != "1" && regOrNot != "2") Console.WriteLine("Invalid input. Try again.");
             }
-            
+
+            Authorization auth = new Authorization();
+            auth.Login(regOrNot);
+
+            String teacherOrNot = "";
+            while (teacherOrNot != "1" && teacherOrNot != "2")
+            {
+                Console.WriteLine("Teacher? Press 1");
+                Console.WriteLine("Student? Press 2");
+                teacherOrNot = Console.ReadLine();
+                if (teacherOrNot != "1" && teacherOrNot != "2") Console.WriteLine("Invalid input. Try again.");
+            }
+            Editor editor = new Editor();
+            if (teacherOrNot == "1")
+            {
+                Console.WriteLine("Logged in as teacher.");
+                editor.printAllTests();
+                int option;
+                bool toContinue = true;
+                while (toContinue)
+                {
+                    Console.WriteLine("Options available:");
+                    Console.WriteLine("1 - add test \n2 - delete test \n3 - edit test name \n4 - print all tests \n5 - edit test \n6 - finish");
+                    Console.WriteLine("Choose option: ");
+                    option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            editor.addTest();
+                            break;
+                        case 2:
+                            editor.deleteTest();
+                            break;
+                        case 3:
+                            editor.editTestName();
+                            break;
+                        case 4:
+                            editor.printAllTests();
+                            break;
+                        case 5:
+                            editor.editTest();
+                            break;
+                        case 6:
+                            toContinue = false;
+                            break;
+                    }
+                }
+            }
         }
     }
 }
--- Editor.cs
+++ Editor.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using project;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace project
 {
@@ -19,7 +22,7 @@ namespace project
         public void printAllTests()
         {
             Console.WriteLine("List of all tests: ");
-
+            if (TestArr.Count == 0) Console.WriteLine("No tests have been created yet.");
             foreach (Test item in TestArr)
             {
                 Console.WriteLine(item.NameOfTest);
@@ -57,6 +60,38 @@ namespace project
             string name = Console.ReadLine();
 
             TestArr[index - 1].NameOfTest = name;
+        }
+
+        public void editTest()
+        {
+            Console.WriteLine("Choose the number of the test you want to edit: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            bool toContinue = true;
+            while (toContinue)
+            {
+                Console.WriteLine("Options available: ");
+                Console.WriteLine("1 - add question to test \n2 - delete question from test \n3 - change place in test \n4 - edit question in test \n5 - finish editing");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        TestArr[index - 1].AddQuestionToTest();
+                        break;
+                    case 2:
+                        TestArr[index - 1].DeleteQuestionFromTest();
+                        break;
+                    case 3:
+                        TestArr[index - 1].ChangePlaceInTest();
+                        break;
+                    case 4:
+                        TestArr[index - 1].EditQuestionInTest();
+                        break;
+                    case 5:
+                        toContinue = false;
+                        break;
+                }
+            }
         }
     }
 }
